4fc8f63b4cc773e65f4f20974ef5d65726a7d460
AnsonWongDtt <46470461+AnsonWongDtt@users.noreply.github.com>
2019-08-30 11:20:22 +0800

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 public class Human{
 	public static void main(String[]args){
-  	System.out.println("Human itself is a problem, and one that we need to solve.");
+  	System.out.println("Manners maketh man.");
 }
 }
